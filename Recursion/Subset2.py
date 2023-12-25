27e073f384497879e14298505a44380e38f01b07
Vushesh Babu Adhikari <vusheshbabuadhikari@Vusheshs-MacBook-Pro.local>
2023-12-25 13:21:25 +0530

    Adds code for subset sum & generating all unique subsets

---
+++ Recursion/Subset2.py
@@ -0,0 +1,58 @@
+# Leetcode link : https://practice.geeksforgeeks.org/problems/subset-sum-ii/1
+# Difficulty: Easy
+# Time Complexity : O(n*2^n)
+# Space Complexity : O(n*2^n)
+
+from typing import List
+
+
+class Solution:
+    def allUniqueSubsets(
+        self,
+        nums: List[int],
+        index: int,
+        temp_arr: List[int],
+        all_subsets: List[List[int]],
+    ) -> None:
+        """Find all the unique subsets of the given array
+
+            - Time complexity: O(n*2^n)
+            - Space complexity: O(n*2^n)
+
+            - Algorithm:
+                - Sort the array
+                - Add the current subset to the list of all subsets
+                - For each element in the array
+                    - If the element is not the first element and is equal to the previous element, skip it
+                    - Add the element to the current subset
+                    - Find all the unique subsets starting from the next element
+                    - Remove the element from the current subset
+                - Return the list of all subsets
+
+        Args:
+            nums (List[int]): Input array
+            index (int): Index of the current element
+            temp_arr (List[int]): Temporary array to store the current subset
+            all_subsets (List[List[int]]): List of all the unique subsets
+        """
+        all_subsets.append(temp_arr.copy())
+        for i in range(index, len(nums)):
+            if i != index and nums[i] == nums[i - 1]:
+                continue
+            temp_arr.append(nums[i])
+            self.allUniqueSubsets(nums, i + 1, temp_arr, all_subsets)
+            temp_arr.pop()
+
+    def printUniqueSubset(self, nums: List[int]):
+        # Code here
+
+        nums.sort()
+        all_unique_subsets = []
+        self.allUniqueSubsets(nums, 0, [], all_unique_subsets)
+
+        return all_unique_subsets
+
+
+if __name__ == "__main__":
+    obj = Solution()
+    print(obj.printUniqueSubset([1, 2, 2]))
---
+++ Recursion/Subset_sum.py
@@ -0,0 +1,44 @@
+# Leetcode link : https://practice.geeksforgeeks.org/problems/subset-sums2234/1
+# Difficulty: Easy
+# Time Complexity : O(2^n)
+from typing import List
+
+
+# User function Template for python3
+class Solution:
+    def subsetSums(self, arr: List[int], N: int) -> List[int]:
+        # code here
+        subset_sums = []
+        self.computeAllSubsetSums(arr, N, 0, 0, subset_sums)
+        subset_sums.sort()
+
+        return subset_sums
+
+    def computeAllSubsetSums(
+        self, arr: List[int], N: int, index: int, cur_sum: int, sum_arr: List[int]
+    ) -> None:
+        """Compute all the subset sums
+
+            - Time complexity: O(2^n)
+            - Space complexity: O(n)
+
+        Args:
+            arr (List[int]): Input array
+            N (int): Length of the input array
+            index (int): Index of the current element
+            cur_sum (int): Current sum
+            sum_arr (List[int]): List of all the subset sums
+        """
+        if index == N:
+            sum_arr.append(cur_sum)
+            return
+
+        # pick an element
+        self.computeAllSubsetSums(arr, N, index + 1, cur_sum + arr[index], sum_arr)
+        # Dont pick the element
+        self.computeAllSubsetSums(arr, N, index + 1, cur_sum, sum_arr)
+
+
+if __name__ == "__main__":
+    obj = Solution()
+    print(obj.subsetSums([2, 3], 2))
